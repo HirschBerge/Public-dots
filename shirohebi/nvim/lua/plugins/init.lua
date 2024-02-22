bbacbd6095f7163b0a09dbaa9e2e44a7989866a4
HirschBerge <hirschy.kirkwood@proton.me>
2024-02-21 22:42:55 -0500

    Fixing NVChad, plus many new plugins recently!

--- shirohebi/nvim/lua/plugins/init.lua
+++ shirohebi/nvim/lua/plugins/init.lua
@@ -70,7 +70,7 @@ local default_plugins = {
   {
     "nvim-treesitter/nvim-treesitter",
     event = { "BufReadPost", "BufNewFile" },
-    tag = "v0.9.2",
+    -- tag = "v0.9.2",
     cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
     build = ":TSUpdate",
     opts = function()
