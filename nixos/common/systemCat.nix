e5f0e5eeb3d4cbf7e6be004aee04747e882de7f6
HirschBerge <hirschy.kirkwood@proton.me>
2024-12-14 18:33:44 -0500

    Updated flake.

--- nixos/common/systemCat.nix
+++ nixos/common/systemCat.nix
@@ -8,7 +8,6 @@ in {
     # this value, nixCats is the defaultPackageName you pass to mkNixosModules
     # it will be the namespace for your options.
     nixCats = {
-      nixpkgs_version = inputs.nixpkgs;
       # these are some of the options. For the rest see :help nixCats.flake.outputs.utils.mkNixosModules you do not need to use every option here, anything you do not define will be pulled from the flake instead.
       enable = true;
       # this will add the overlays from ./overlays and also, add any plugins in inputs named "plugins-pluginName" to pkgs.neovimPlugins It will not apply to overall system, just nixCats.
