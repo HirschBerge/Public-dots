413eb45d28e270dbe4b1aafbdbc0ff542e83d939
HirschBerge <hirschy.kirkwood@proton.me>
2025-02-15 01:08:20 -0500

    for some reason with nu update lost my path additions

--- nixos/common/configs/zsh.nix
+++ nixos/common/configs/zsh.nix
@@ -183,8 +183,9 @@ in {
         $env.config = { edit_mode: vi, show_banner: false,}
         $env.PROMPT_INDICATOR_VI_INSERT = " "
         $env.PROMPT_INDICATOR_VI_NORMAL = "❮ "
+        $env.PATH = $env.PATH | split row (char esep) |append $"($env.HOME)/.local/bin"
         export-env { $env.FZF_DEFAULT_OPTS = " --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc  --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8  --color=selected-bg:#45475a  --multi --preview 'fzf_previewer {}' --ansi"}
-        export-env { $env.FZF_DEFAULT_COMMAND="fd --strip-cwd-prefix --exclude"}
+        export-env { $env.FZF_DEFAULT_COMMAND = "fd --strip-cwd-prefix --exclude"}
         source ~/.zoxide.nu
         source ~/.config/nushell/satty.nu
         def fo [ ] {
